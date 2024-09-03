ce9b089b6c3aee67447af92e6370227bdb2689d5
R4Rishi <all4rishi@gmail.com>
2024-09-03 14:23:31 +0530

    Add files via upload

---
+++ my-ecommerce-project/backend/config.py
@@ -0,0 +1,6 @@
+# config.py
+
+AWS_ACCESS_KEY_ID = 'YOUR_ACCESS_KEY_ID'
+AWS_SECRET_ACCESS_KEY = 'YOUR_SECRET_ACCESS_KEY'
+S3_BUCKET_NAME = 'product-details-bucket'
+DYNAMODB_TABLE_NAME = 'orders'  # DynamoDB table for orders
