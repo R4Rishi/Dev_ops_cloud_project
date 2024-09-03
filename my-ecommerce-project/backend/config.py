ceb5bcd155877057d1c303f12249113e22b1a517
R4Rishi <all4rishi@gmail.com>
2024-09-03 14:57:43 +0530

    Delete my-ecommerce-project directory

--- my-ecommerce-project/backend/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-AWS_ACCESS_KEY_ID = 'YOUR_ACCESS_KEY_ID'
-AWS_SECRET_ACCESS_KEY = 'YOUR_SECRET_ACCESS_KEY'
-S3_BUCKET_NAME = 'product-details-bucket'
-DYNAMODB_TABLE_NAME = 'orders'  # DynamoDB table for orders
